571cb916ec7ee53e1487185e8a53056a6577c0ad
Rik <161947286+RiquelmeLadislau@users.noreply.github.com>
2024-09-18 22:00:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+    let answer = (prompt(`Selecione uma das opções
+    1 - continuar
+    2 - continuar
+    3 - continuar
+    4 - continuar
+    5 - encerrar`))
+    do{
+    switch (answer) {
+        case 1:
+            alert('Você escolheu continuar')
+            break
+        case 2:
+            alert('Você escolheu continuar')
+            break
+        case 3:
+            alert('Você escolheu continuar')
+            break
+        case 4:
+            alert('Você escolheu continuar')
+            break
+    }
+} while(answer != 5)
